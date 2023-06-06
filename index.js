186dd5b98e0ebeab47b04516172a27cde289b23e
mr1mokhtari <mr1mokhtari@gmail.com>
2023-06-06 10:34:13 +0430

    3.5-6 phonebook step5-6 post single entry & error handling

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.json()); //it takes the JSON data of a request, transforms it into a JavaScript object and then attaches it to the body property of the request object before the route handler is called.
 let persons = [
   {
     id: 1,
@@ -26,8 +27,7 @@ app.get('/api/persons', (request, response) => {
   response.json(persons);
 });
 app.get('/info', (request, response) => {
-  let time = new Date();
-  time = time.toString();
+  let time = new Date().toString();
   response.send(
     `<div><br/><p>Phonebook has info for ${persons.length} people</p><p>${time}</p></div>`
   );
@@ -42,5 +42,18 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter((p) => p.id !== id);
   response.status(204).end();
 });
+app.post('/api/persons', (request, response) => {
+  const { name, number } = request.body;
+  const err = (error) =>
+    response.status(400).json({
+      error,
+    });
+  !(name && number) && err('name or number missing');
+  persons.find((p) => p.name === name) && err('Name must be unique');
+  const person = { name, number, id: Math.trunc(Math.random() * 10000) };
+  persons = persons.concat(person);
+  response.json(person);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
