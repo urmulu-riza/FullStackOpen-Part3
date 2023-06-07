ded8021d62f96ae2d9920a1fc9c0f573114404ae
mr1mokhtari <mr1mokhtari@gmail.com>
2023-06-07 10:45:48 +0430

    3.9 phonebook backend Step9 Deployment to Render

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ morgan.token('body', (req) =>
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 );
+const cors = require('cors');
 app.use(cors());
+app.use(express.static('build'));
 
 let persons = [
   {
