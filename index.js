0525973c95895e308650ff2c8c51c49a84d1cc8a
mr1mokhtari <mr1mokhtari@gmail.com>
2023-06-10 09:25:06 +0430

    3.15 phonebook database Step3 deleting entries

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+
 const Person = require('./models/person');
 
-app.use(express.json()); //takes the JSON data of a req, transforms into JS obj & attaches it to the body property of the req obj before the route handler is called
+app.use(express.static('build'));
+app.use(express.json()); //takes the JSON data of a req, transforms 2 JSobj & attaches 2 the req.body
 //Morgan
+
 const morgan = require('morgan');
 // app.use(morgan('tiny'));
 morgan.token('body', (req) =>
@@ -14,32 +17,9 @@ morgan.token('body', (req) =>
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 );
+
 const cors = require('cors');
 app.use(cors());
-app.use(express.static('build'));
-
-// let persons = [
-//   {
-//     id: 1,
-//     name: 'Arto Hellas',
-//     number: '040-123456',
-//   },
-//   {
-//     id: 2,
-//     name: 'Ada Lovelace',
-//     number: '39-44-5323523',
-//   },
-//   {
-//     id: 3,
-//     name: 'Dan Abramov',
-//     number: '12-43-234345',
-//   },
-//   {
-//     id: 4,
-//     name: 'Mary Poppendieck',
-//     number: '39-23-6423122',
-//   },
-// ];
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then((persons) => response.json(persons));
@@ -51,16 +31,20 @@ app.get('/api/persons', (request, response) => {
 //   );
 // });
 app.get('/api/persons/:id', (request, response) => {
-  // const id = Number(request.params.id);
-  // const person = persons.find((p) => p.id === id);
-  // person ? response.json(person) : response.status(404).end();
-  Person.findById(request.params.id).then((person) => response.json(person));
+  Person.findById(request.params.id)
+    .then((person) =>
+      person ? response.json(person) : response.status(404).end()
+    )
+    .catch((error) => {
+      console.log(error);
+      response.status(400).send({ error: 'malformatted id' });
+    });
 });
-// app.delete('/api/persons/:id', (request, response) => {
-//   const id = Number(request.params.id);
-//   persons = persons.filter((p) => p.id !== id);
-//   response.status(204).end();
-// });
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => response.status(204).end())
+    .catch((error) => next(error));
+});
 app.post('/api/persons', (request, response) => {
   const { name, number } = request.body;
 
