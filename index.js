c3ddb59d97e4496fed27efe1bf1bd8ef85f2fcfd
mr1mokhtari <mr1mokhtari@gmail.com>
2023-06-10 08:02:39 +0430

    3.13-14 phonebook database fetching & saving entries

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const Person = require('./models/person');
+
 app.use(express.json()); //takes the JSON data of a req, transforms into JS obj & attaches it to the body property of the req obj before the route handler is called
 //Morgan
 const morgan = require('morgan');
@@ -15,47 +18,49 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.static('build'));
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
+// let persons = [
+//   {
+//     id: 1,
+//     name: 'Arto Hellas',
+//     number: '040-123456',
+//   },
+//   {
+//     id: 2,
+//     name: 'Ada Lovelace',
+//     number: '39-44-5323523',
+//   },
+//   {
+//     id: 3,
+//     name: 'Dan Abramov',
+//     number: '12-43-234345',
+//   },
+//   {
+//     id: 4,
+//     name: 'Mary Poppendieck',
+//     number: '39-23-6423122',
+//   },
+// ];
+
 app.get('/api/persons', (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => response.json(persons));
 });
-app.get('/info', (request, response) => {
-  let time = new Date().toString();
-  response.send(
-    `<div><br/><p>Phonebook has info for ${persons.length} people</p><p>${time}</p></div>`
-  );
-});
+// app.get('/info', (request, response) => {
+//   let time = new Date().toString();
+//   response.send(
+//     `<div><br/><p>Phonebook has info for ${persons.length} people</p><p>${time}</p></div>`
+//   );
+// });
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((p) => p.id === id);
-  person ? response.json(person) : response.status(404).end();
+  // const id = Number(request.params.id);
+  // const person = persons.find((p) => p.id === id);
+  // person ? response.json(person) : response.status(404).end();
+  Person.findById(request.params.id).then((person) => response.json(person));
 });
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((p) => p.id !== id);
-  response.status(204).end();
-});
+// app.delete('/api/persons/:id', (request, response) => {
+//   const id = Number(request.params.id);
+//   persons = persons.filter((p) => p.id !== id);
+//   response.status(204).end();
+// });
 app.post('/api/persons', (request, response) => {
   const { name, number } = request.body;
 
@@ -64,21 +69,22 @@ app.post('/api/persons', (request, response) => {
       error: 'name or number missing',
     });
   }
-  if (persons.find((p) => p.name === name)) {
-    return response.status(422).json({
-      error: 'Name must be unique',
-    });
-  }
+  // if (persons.find((p) => p.name === name)) {
+  //   return response.status(422).json({
+  //     error: 'Name must be unique',
+  //   });
+  // }
   if (request.header('Content-Type') !== 'application/json') {
     return res.status(400).json({ error: 'Content-Type Unsupported' });
   }
-  const person = { name, number, id: Math.trunc(Math.random() * 10000) };
-  persons = persons.concat(person);
-  response.json(person);
+  const person = new Person({ name, number });
+  person.save().then((savedPerson) => response.json(savedPerson));
 });
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' });
 };
 app.use(unknownEndpoint);
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
